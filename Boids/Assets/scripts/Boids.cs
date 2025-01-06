e882a95566e67f244b27c093f34e2145655152e9
Ofish <vc.ofir@gmail.com>
2025-01-05 23:40:52 -0500

    tried to add birdView, didn't work

--- Boids/Assets/scripts/Boids.cs
+++ Boids/Assets/scripts/Boids.cs
@@ -8,6 +8,9 @@ public class Boids : MonoBehaviour
     private Transform transform;
     private GameObject[] boids;
 
+    private float viewRadius = 5.0f;
+    private float viewLengh = 10.0f;
+
     private Vector3 separation;
     private Vector3 alignment;
     private Vector3 cohesion;
@@ -22,6 +25,7 @@ public class Boids : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int inView = 0;
         foreach (GameObject boid in boids)
         {
             if (boid == gameObject)
@@ -29,28 +33,52 @@ public class Boids : MonoBehaviour
                 continue;
             }
 
-            alignment += boid.GetComponent<Boids>().steer;
 
+            if(InFieldOfView(boid)) {
+                inView++;
+                alignment += boid.GetComponent<Boids>().steer;
+                alignment += boid.GetComponent<Boids>().steer;
+            }
         }
 
-        // print(alignment);
-        if (boids.Length > 2)
-            alignment = alignment / (boids.Length -1);
+        print("inView: " + inView);
+        if (inView > 1) {
+            alignment = alignment / inView;
             steer = Vector3.Lerp(steer, alignment, Time.deltaTime);
-
+        }
 
         transform.position +=  steer * Time.deltaTime;
         transform.rotation = Quaternion.LookRotation(steer);
         
     }
 
-    private Vector3 Seperation() {
-        return Vector3.zero;
+    private bool InFieldOfView(GameObject boid)
+    {
+        Vector3 coneStartingPoint = transform.position;
+        Vector3 coneBaseCenter = transform.position + transform.forward * viewLengh;
+        Vector3 normalizedConeDir = Vector3.Normalize(coneBaseCenter - coneStartingPoint);
+        Vector3 pointToaxis = boid.transform.position - coneStartingPoint;
+        float Projection = Vector3.Dot(pointToaxis, normalizedConeDir);
+
+        print("Projection: " + Projection);
+
+
+        if (Projection < 0 || Projection > viewLengh)
+        {
+            return false;
+        }
+        float radicalDistance = Vector3.Magnitude(pointToaxis - Projection * normalizedConeDir);
+        float projectionRadius = (viewRadius/viewLengh) * Projection;
+
+        print("radicalDistance: " + radicalDistance);
+        print("projectionRadius: " + projectionRadius);
+
+        if (radicalDistance < projectionRadius)
+        {
+            return true;
+        }
+        return false;
     }
 
 
-
-    private Vector3 Cohesion() {
-        return Vector3.zero;
-    }
 }
