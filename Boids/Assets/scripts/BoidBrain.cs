0b6aa1ca9d152b8278faf34afc603be7d153e9eb
Ofish <vc.ofir@gmail.com>
2025-01-19 16:09:21 -0500

    object avoidance working mabye without slowing down

--- Boids/Assets/scripts/BoidBrain.cs
+++ Boids/Assets/scripts/BoidBrain.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using NUnit.Framework;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Timeline;
 
@@ -9,6 +11,8 @@ public class BoidBrain : MonoBehaviour
     public Vector3 steer;
     public float inView;
     [HideInInspector] public float maxSpeed;
+
+    private float speed;
     private GameObject[] boids;
     [HideInInspector] public float viewLengh;
 
@@ -32,8 +36,9 @@ public class BoidBrain : MonoBehaviour
 
     void Start()
     {
+        speed = maxSpeed;
         boids = GameObject.FindGameObjectsWithTag("boid");
-        steer = new Vector3(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed)).normalized * maxSpeed;
+        steer = new Vector3(Random.Range(-speed, speed), Random.Range(-speed, speed), Random.Range(-speed, speed)).normalized * speed;
     }
 
     // Update is called once per frame
@@ -68,8 +73,6 @@ public class BoidBrain : MonoBehaviour
         if (inView > 0) {
             alignment = alignment / inView;
             cohesion = cohesion / inView;
-            // steer = Vector3.Slerp(steer, cohesion, Time.deltaTime);
-            // steer = Vector3.Slerp(steer, -separation, Time.deltaTime);
         }
 
         obsticleAvoidance = ObsticleAvoidance();
@@ -77,7 +80,7 @@ public class BoidBrain : MonoBehaviour
 
 
 
-        weigtedForces = weigtedForces.normalized * maxSpeed;
+        weigtedForces = weigtedForces.normalized * speed;
 
         
 
@@ -110,58 +113,101 @@ public class BoidBrain : MonoBehaviour
         return true;
     }
 
+
     private Vector3 ObsticleAvoidance()
     {
-        // Ray ray = new Ray(transform.position, transform.forward * viewLengh); 
-        // Debug.DrawRay(ray.origin, ray.direction * viewLengh, Color.green);
-        if(Physics.SphereCast(transform.position, sphereColliderRadius, transform.forward, out RaycastHit hit, viewLengh + sphereColliderRadius))
+        float bestAngle = 180.0f;
+        Vector3 besrtDir = transform.forward;
+
+        Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, viewLengh);
+        if (hit.collider == null)
         {
-            Debug.Log(hit.collider.gameObject.name);
-            float bestAngle = 360;
-            float ExtremeAngle = 0;
-            Vector3 bestDirection = Vector3.zero;
-            Vector3 ExtremeDirection = Vector3.zero;
-
-            for (int i = 0; i < rayCount; i++)
-            {
-                float indices = i + .5f;
-                float phi = Mathf.Acos(1-2*indices/rayCount);
-                float theta = Mathf.PI * (1 + Mathf.Sqrt(5)) * indices;
-
-                Vector3 normalRay =new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Sin(theta) * Mathf.Sin(phi), Mathf.Cos(phi));
-
-                Physics.Raycast(transform.position, normalRay, out RaycastHit hit2, viewLengh);
-                // Debug.DrawRay(transform.position, normalRay * viewLengh, Color.blue);
-                if (Vector3.Angle(transform.forward, normalRay) > fieldOfView)
-                {
-                    Debug.DrawRay(transform.position, normalRay * viewLengh, Color.red);
-                    continue;
-                }
-                if (Vector3.Angle(transform.forward, normalRay) > ExtremeAngle)
-                {
-                    Debug.DrawRay(transform.position, normalRay * viewLengh, Color.blue);
-                    ExtremeDirection = normalRay;
-                    ExtremeAngle = Vector3.Angle(transform.forward, normalRay);
-                }
-                if (hit2.collider != hit.collider && Vector3.Angle(transform.forward, normalRay) < bestAngle)
-                {
-                    Debug.DrawRay(transform.position, normalRay * viewLengh, Color.green);
-                    bestDirection = normalRay;
-                    bestAngle = Vector3.Angle(transform.forward, normalRay);
-
-                }
-
-            }
-            if (bestDirection != Vector3.zero)
-            {
-                Debug.DrawRay(transform.position, bestDirection *  viewLengh, Color.black);
-                return bestDirection;
-            }
-            Debug.DrawRay(transform.position, ExtremeDirection * viewLengh, Color.yellow);
-            return ExtremeDirection ;
+            return Vector3.zero;
         }
-        return Vector3.zero;
+
+        Debug.Log("hit: " + hit.collider.gameObject.name);
+        Debug.DrawRay(transform.position, transform.forward * viewLengh, Color.white);
+        for (int i = 0; i < rayCount; i++)
+        {
+            float indices = i + .5f;
+            float phi = Mathf.Acos(1-2*indices/rayCount);
+            float theta = Mathf.PI * (1 + Mathf.Sqrt(5)) * indices;
+
+            Vector3 normalRay = new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Sin(theta) * Mathf.Sin(phi), Mathf.Cos(phi)).normalized;
+
+            if(Physics.Raycast(transform.position, normalRay, out RaycastHit hit2, viewLengh * 2f)) 
+
+            if (hit2.collider != null)
+            {
+                continue;
+            }
+            if (Vector3.Angle(transform.forward, normalRay) > fieldOfView)
+            {
+                continue;
+            }
+            if (Vector3.Angle(transform.forward, normalRay) < bestAngle)
+            {
+                besrtDir = normalRay;
+                bestAngle = Vector3.Angle(transform.forward, normalRay);
+            }
+
+            Debug.DrawRay(transform.position, normalRay * viewLengh * 2f, Color.green);
+        }
+        return besrtDir;
     }
+
+    // private Vector3 ObsticleAvoidance()
+    // {
+    //     // Ray ray = new Ray(transform.position, transform.forward * viewLengh); 
+    //     // Debug.DrawRay(ray.origin, ray.direction * viewLengh, Color.green);
+    //     // if(Physics.SphereCast(transform.position, sphereColliderRadius, transform.forward, out RaycastHit hit, viewLengh + sphereColliderRadius))
+    //     // {
+    //         Debug.Log(hit.collider.gameObject.name);
+    //         float mediumAngle = 360;
+    //         float ExtremeAngle = 0;
+    //         Vector3 mediumDirection = Vector3.zero;
+    //         Vector3 ExtremeDirection = Vector3.zero;
+
+    //         for (int i = 0; i < rayCount; i++)
+    //         {
+    //             float indices = i + .5f;
+    //             float phi = Mathf.Acos(1-2*indices/rayCount);
+    //             float theta = Mathf.PI * (1 + Mathf.Sqrt(5)) * indices;
+
+    //             Vector3 normalRay =new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Sin(theta) * Mathf.Sin(phi), Mathf.Cos(phi));
+
+    //             Physics.Raycast(transform.position, normalRay, out RaycastHit hit2, viewLengh);
+    //             // Debug.DrawRay(transform.position, normalRay * viewLengh, Color.blue);
+    //             if (Vector3.Angle(transform.forward, normalRay) > fieldOfView)
+    //             {
+    //                 Debug.DrawRay(transform.position, normalRay * viewLengh, Color.red);
+    //                 continue;
+    //             }
+    //             if (Vector3.Angle(transform.forward, normalRay) > ExtremeAngle)
+    //             {
+    //                 Debug.DrawRay(transform.position, normalRay * viewLengh, Color.blue);
+    //                 ExtremeDirection = normalRay;
+    //                 ExtremeAngle = Vector3.Angle(transform.forward, normalRay);
+    //             }
+    //             if (hit2.collider != hit.collider && Vector3.Angle(transform.forward, normalRay) < mediumAngle)
+    //             {
+    //                 Debug.DrawRay(transform.position, normalRay * viewLengh, Color.green);
+    //                 mediumDirection = normalRay;
+    //                 mediumAngle = Vector3.Angle(transform.forward, normalRay);
+
+    //             }
+
+    //         }
+    //         if (mediumDirection != Vector3.zero)
+    //         {
+    //             Debug.DrawRay(transform.position, mediumDirection *  viewLengh, Color.black);
+    //             return mediumDirection;
+    //         }
+    //         Debug.DrawRay(transform.position, ExtremeDirection * viewLengh, Color.yellow);
+    //         return ExtremeDirection ;
+    //     // }
+    //     return Vector3.zero;
+    // }
 
 
 
